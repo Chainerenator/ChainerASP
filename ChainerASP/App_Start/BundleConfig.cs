f04f79ae07d093b14320a1418e1ae627cff940ed
KB Chainer <chainerenator@gmail.com>
2018-08-03 14:43:39 +0300

    newshit

--- ChainerASP/App_Start/BundleConfig.cs
+++ ChainerASP/App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace ChainerASP
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lux.css",
                       "~/Content/site.css"));
         }
     }
---
+++ ChainerASP/Controllers/MoviesController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using ChainerASP.Models;
+
+namespace ChainerASP.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies/Random           
+        public ActionResult Random()
+        {
+            var movie = new Movie() {Name = "Shrek!"};
+           // return View(movie);
+            //return Content("IDI NAHUY");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy= "name"}); 
+        }
+    }
+}
