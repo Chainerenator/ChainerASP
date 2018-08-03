eedf4689a98096a82e6a5620bfbe382d76b3c645
KB Chainer <chainerenator@gmail.com>
2018-08-03 18:06:14 +0300

    some txt

---
+++ ChainerASP/Controllers/SharedController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace ChainerASP.Controllers
+{
+    public class SharedController : Controller
+    {
+        public ActionResult Index(string s)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
